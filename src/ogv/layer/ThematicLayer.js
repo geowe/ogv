9dc9f8f169ee45d094f9b5cd3b12e7eef2fd4bfa
jmmluna <jmmluna@gmail.com>
2019-11-26 09:35:31 +0100

    Versión 2.13.1

--- src/ogv/layer/ThematicLayer.js
+++ src/ogv/layer/ThematicLayer.js
@@ -5,123 +5,113 @@ import { ThematicLayerLegend } from '../legend/ThematicLayerLegend';
 
 const FONT = 'bold 12px "Open Sans", "Arial Unicode MS", "sans-serif"';
 export class ThematicLayer extends BaseLayer {
-    constructor() {
-        super([], LayerTypeName.THEMATIC_LAYER);
-        this._style = new LayerStyle();
-    }
+  constructor () {
+    super([], LayerTypeName.THEMATIC_LAYER);
+    this._style = new LayerStyle();
+  }
 
-    setSetting(layerSetting) {
-        this._map = layerSetting.map;
-        this._attribute = layerSetting[LayerTypeName.THEMATIC_LAYER].attribute;
-        this._label = layerSetting[LayerTypeName.THEMATIC_LAYER].label;
-    }
+  setSetting (layerSetting) {
+    this._map = layerSetting.map;
+    this._attribute = layerSetting[LayerTypeName.THEMATIC_LAYER].attribute;
+    this._label = layerSetting[LayerTypeName.THEMATIC_LAYER].label;
+  }
 
-    setFeatures(features) {
-        super.setFeatures(features);
-        this.createCategories();
-        this.setLayerLegend(new ThematicLayerLegend({
-            categories: this._rule,
-            parent: this._map.getOverlayContainerStopEvent()
-        }));
-    }
+  setFeatures (features) {
+    super.setFeatures(features);
+    this.createCategories();
+    this.setLayerLegend(new ThematicLayerLegend({
+      categories: this._rule,
+      parent: this._map.getOverlayContainerStopEvent()
+    }));
+  }
 
-    createCategories() {
-        this._rule = this.createThematicRule();
-        const keys = Object.keys(this._rule);
-        this._style.setTotalColorPalette(keys.length);
-        const numberKeys = this.convertToNumber(keys);
-        this._rule = this.getOrderlyRule(numberKeys);
-    }
+  createCategories () {
+    this._rule = this.createThematicRule();
+    const keys = Object.keys(this._rule);
+    this._style.setTotalColorPalette(keys.length);
+    const numberKeys = this.convertToNumber(keys);
+    this._rule = this.getOrderlyRule(numberKeys);
+  }
 
-    getLayer() {
-        super.getLayer();
-        this._layer.setStyle((feature) => {
-            const category = this._rule[feature.get(this._attribute)];
+  getLayer () {
+    super.getLayer();
+    this._layer.setStyle((feature) => {
+      const category = this._rule[feature.get(this._attribute)];
 
-            if (category === undefined) {
-                return null;
-            }
+      if (category === undefined) {
+        return null;
+      }
 
-            const style = category.style;
-            if (style !== undefined) {
-                if (this._label !== undefined) {
-                    style.getText().setFont(FONT);
-                    style.getText().setText("" + feature.get(this._label));
-                }
-            }
-
-            return style;
-        });
-
-        return this._layer;
-    }
-
-    createThematicRule() {
-        const rule = {};
-
-        this._features.forEach((feature) => {
-            const value = feature.get(this._attribute);
-            //let label = '' + value;
-
-            //if (this._label !== undefined) { label = feature.get(this._label); }
-
-            //label = (label === undefined) ? '' : label;
-            if (value !== null && !(value in rule)) {
-                rule[value] = {
-                    //label: '' + label,
-                    count: 1
-                };
-            } else if (value !== null && (value in rule)) {
-                let count = parseInt(rule[value].count);
-                count = count + 1;
-                rule[value].count = count;
-            }
-        });
-
-        return rule;
-    }
-
-    convertToNumber(keys) {
-        var isNumeric = keys.every(function(e) {
-            return !isNaN(e);
-        });
-
-        if (isNumeric) {
-            return keys.map(Number);
-        } else {
-            return keys;
+      const style = category.style;
+      if (style !== undefined) {
+        if (this._label !== undefined) {
+          style.getText().setFont(FONT);
+          style.getText().setText('' + feature.get(this._label));
         }
+      }
+
+      return style;
+    });
+
+    return this._layer;
+  }
+
+  createThematicRule () {
+    const rule = {};
+
+    this._features.forEach((feature) => {
+      const value = feature.get(this._attribute);
+
+      if (value !== null && !(value in rule)) {
+        rule[value] = {
+          count: 1
+        };
+      } else if (value !== null && (value in rule)) {
+        let count = parseInt(rule[value].count);
+        count = count + 1;
+        rule[value].count = count;
+      }
+    });
+
+    return rule;
+  }
+
+  convertToNumber (keys) {
+    var isNumeric = keys.every(function (e) {
+      return !isNaN(e);
+    });
+
+    if (isNumeric) {
+      return keys.map(Number);
+    } else {
+      return keys;
     }
+  }
 
-    getOrderlyRule(keys) {
-        const orderlyRule = {};
-        const orderedKeys = keys.sort(function(a, b) {
-            if (a > b) {
-                return 1;
-            }
-            if (a < b) {
-                return -1;
-            }
+  getOrderlyRule (keys) {
+    const orderlyRule = {};
+    const orderedKeys = keys.sort(function (a, b) {
+      if (a > b) {
+        return 1;
+      }
+      if (a < b) {
+        return -1;
+      }
 
-            return 0;
-        });
+      return 0;
+    });
 
-        orderedKeys.forEach((key) => {
-            const ruleValue = this._rule[key];
-            if (ruleValue !== undefined) {
-                //const textLabel = ruleValue.label;
+    orderedKeys.forEach((key) => {
+      const ruleValue = this._rule[key];
+      if (ruleValue !== undefined) {
+        orderlyRule['' + key] = {
+          count: ruleValue.count,
+          style: this._style.getNextStyle()
+        };
+      }
+    });
 
-                orderlyRule['' + key] = {
-                    //label: textLabel,
-                    count: ruleValue.count,
-                    style: this._style.getNextStyle()
-                };
-
-                //if (this._label !== undefined) { orderlyRule['' + key].style.getText().setText(textLabel); }
-            }
-        });
-
-        orderlyRule.orderedKeys = orderedKeys;
-        return orderlyRule;
-    }
+    orderlyRule.orderedKeys = orderedKeys;
+    return orderlyRule;
+  }
 }
--- src/ui/FeatureInfoPanel.js
+++ src/ui/FeatureInfoPanel.js
@@ -8,129 +8,102 @@ import { GeoJSONPanel } from './GeoJSONPanel';
 import { VisualElement } from './VisualElement';
 
 export class FeatureInfoPanel extends VisualElement {
-    constructor(arg) {
-        super(arg.parent, html);
+  constructor (arg) {
+    super(arg.parent, html);
 
-        this._content = document.getElementById('content');
-        this._spatialContent = document.getElementById('spatialContent');
-        this._wktButton = document.getElementById('wktButton');
-        this._featureButton = document.getElementById('featureButton');
-        const closeButton = document.getElementById('closeContentButton');
+    this._content = document.getElementById('content');
+    this._spatialContent = document.getElementById('spatialContent');
+    this._wktButton = document.getElementById('wktButton');
+    this._featureButton = document.getElementById('featureButton');
+    const closeButton = document.getElementById('closeContentButton');
 
-        closeButton.onclick = () => {
-            // document.getElementById('elementContent').style.display = 'none';
-            this.hide();
-        };
+    closeButton.onclick = () => {
+      this.hide();
+    };
 
-        this.wktPanel = new WKTPanel({
-            parent: this._element
+    this.wktPanel = new WKTPanel({
+      parent: this._element
+    });
+
+    this.geojsonPanel = new GeoJSONPanel({
+      parent: this._element
+    });
+  }
+
+  show (feature) {
+    this._content.innerHTML = '';
+    this.setAlphanumericInfo(feature);
+    this.spatialInfo(feature);
+    super.show();
+  }
+
+  hide () {
+    super.hide();
+    if (this.wktPanel !== undefined && this.geojsonPanel !== undefined) { this.wktPanel.hide(); }
+  }
+
+  setAlphanumericInfo (feature) {
+    var originalFeature = feature.get('features');
+
+    if (originalFeature !== undefined) {
+      if (originalFeature.length > 1) {
+        var featureGroup = new Feature({
+          ELEMENTO: 'Grupo',
+          'Nº ELEMENTOS': originalFeature.length
         });
 
-        this.geojsonPanel = new GeoJSONPanel({
-            parent: this._element
-        });
+        feature = featureGroup;
+      } else {
+        feature = originalFeature[0];
+      }
     }
 
-    show(feature) {
-        this._content.innerHTML = '';
-        this.setAlphanumericInfo(feature);
-        this.spatialInfo(feature);
-        super.show();
+    const objeto = feature.getProperties();
+
+    let imageUrl = feature.get(IMAGE_FIELD_NAME);
+    if (imageUrl !== undefined && imageUrl !== null) {
+      if (imageUrl.substring(0, 4) === IMAGE_VIEWER_FIELD) {
+        imageUrl = imageUrl.substring(4);
+      }
+
+      this._content.innerHTML = `<a target="_blank" href="${imageUrl}"><img class="w3-card-4" src="${imageUrl}" width="100%" height="100%" > </a><br /><br/>`;
     }
 
-    hide() {
-        super.hide();
-        if (this.wktPanel !== undefined && this.geojsonPanel !== undefined) { this.wktPanel.hide(); }
-    }
-
-    setAlphanumericInfo(feature) {
-        var originalFeature = feature.get('features');
-
-        if (originalFeature !== undefined) {
-            if (originalFeature.length > 1) {
-                var featureGroup = new Feature({
-                    ELEMENTO: 'Grupo',
-                    'Nº ELEMENTOS': originalFeature.length
-                });
-
-                feature = featureGroup;
-            } else {
-                feature = originalFeature[0];
-            }
-        }
-
-        const objeto = feature.getProperties();
-
-        let imageUrl = feature.get(IMAGE_FIELD_NAME);
-        if (imageUrl !== undefined && imageUrl !== null) {
-            if (imageUrl.substring(0, 4) === IMAGE_VIEWER_FIELD) {
-                imageUrl = imageUrl.substring(4);
-            }
-
-            this._content.innerHTML = `<a target="_blank" href="${imageUrl}"><img class="w3-card-4" src="${imageUrl}" width="100%" height="100%" > </a><br /><br/>`;
-        }
-
-        for (const propiedades in objeto) {
-
-            /*if (propiedades.includes("-")) {
-                let table = "<table>";
-                let header = propiedades.split("-");
-
-
-                table += `<tr>`;
-                header.forEach((field) => {
-                    table += `<th>${field}</th>`;
-                })
-
-                table += `</tr>`;
-
-                let values = objeto[propiedades].split("-");
-
-                table += `<tr>`;
-                values.forEach((field) => {
-                    table += `<td>${field}</td>`;
-                })
-                table += `</tr>`;
-
-                table += "</table>";
-
-                this._content.innerHTML += `<label><b>${propiedades}</b></label> <br/>${table}`;
-
-            } else*/
-            if (propiedades !== 'geometry' && propiedades !== IMAGE_FIELD_NAME) {
-                this._content.innerHTML += `<label><b>${propiedades}</b></label> <br/>${objeto[propiedades]}<br><div style="width: 100%;                
+    for (const propiedades in objeto) {
+      if (propiedades !== 'geometry' && propiedades !== IMAGE_FIELD_NAME) {
+        this._content.innerHTML += `<label><b>${propiedades}</b></label> <br/>${objeto[propiedades]}<br><div style="width: 100%;                
                 display: block;margin-top: 0.1em;margin-bottom: 0.1em;margin-left: auto;margin-right: auto;border-style: solid;border-width: 1px;border-color:#e8ebe9;">`;
-            }
-        }
+      }
+    }
+  }
+
+  spatialInfo (feature) {
+    this._wktButton.onclick = () => {
+      this.geojsonPanel.hide();
+      this.wktPanel.show(feature);
+    };
+
+    this._featureButton.onclick = () => {
+      this.wktPanel.hide();
+      this.geojsonPanel.show(feature);
+    };
+
+    const geometry = feature.getGeometry().clone().transform('EPSG:3857', 'EPSG:4326');
+    this._spatialContent.innerHTML = '<b>Tipo</b> <br/>' + geometry.getType() + '<br />';
+    const coordinates = geometry.getCoordinates();
+
+    if (geometry.getType() === 'Point') {
+      this._spatialContent.innerHTML += `<b>X</b> <br/>${coordinates[0]}<br />`;
+      this._spatialContent.innerHTML += `<b>Y</b> <br/>${coordinates[1]}<br />`;
+      this._spatialContent.innerHTML += `<b>HDMS</b> <br/>${toStringHDMS(coordinates, 1)}<br />`;
+    } else if (geometry.getType() === 'Polygon' || geometry.getType() === 'MultiPolygon') {
+      this._spatialContent.innerHTML += `<b>Área(m<sup>2</sup>)</b> <br/>${getArea(geometry, { projection: 'EPSG:4326' }).toFixed(2)}<br />`;
+      this._spatialContent.innerHTML += `<b>Vértices</b> <br/>${coordinates[0].length}<br />`;
+    } else if (geometry.getType() === 'LineString') {
+      this._spatialContent.innerHTML += `<b>Longitud(m<sup>2</sup>)</b> <br/>${getLength(geometry, { projection: 'EPSG:4326' }).toFixed(2)}<br />`;
+      this._spatialContent.innerHTML += `<b>Vértices</b> <br/>${coordinates[0].length}<br />`;
     }
 
-    spatialInfo(feature) {
-        this._wktButton.onclick = () => {
-            this.geojsonPanel.hide();
-            this.wktPanel.show(feature);
-        };
-
-        this._featureButton.onclick = () => {
-            this.wktPanel.hide();
-            this.geojsonPanel.show(feature);
-        };
-
-        const geometry = feature.getGeometry().clone().transform('EPSG:3857', 'EPSG:4326');
-        this._spatialContent.innerHTML = '<b>Tipo</b> <br/>' + geometry.getType() + '<br />';
-        const coordinates = geometry.getCoordinates();
-
-        if (geometry.getType() === 'Point') {
-            this._spatialContent.innerHTML += `<b>X</b> <br/>${coordinates[0]}<br />`;
-            this._spatialContent.innerHTML += `<b>Y</b> <br/>${coordinates[1]}<br />`;
-            this._spatialContent.innerHTML += `<b>HDMS</b> <br/>${toStringHDMS(coordinates, 1)}<br />`;
-        } else if (geometry.getType() === 'Polygon' || geometry.getType() === 'MultiPolygon') {
-            this._spatialContent.innerHTML += `<b>Área(m<sup>2</sup>)</b> <br/>${getArea(geometry, { projection: 'EPSG:4326' }).toFixed(2)}<br />`;
-            this._spatialContent.innerHTML += `<b>Vértices</b> <br/>${coordinates[0].length}<br />`;
-        } else if (geometry.getType() === 'LineString') {
-            this._spatialContent.innerHTML += `<b>Longitud(m<sup>2</sup>)</b> <br/>${getLength(geometry, { projection: 'EPSG:4326' }).toFixed(2)}<br />`;
-            this._spatialContent.innerHTML += `<b>Vértices</b> <br/>${coordinates[0].length}<br />`;
-        }
-
-        this._spatialContent.innerHTML += '<br />';
-    }
+    this._spatialContent.innerHTML += '<br />';
+  }
 }
