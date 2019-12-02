39cc77368a40647ba65d798114d0a960b0fb2f7d
Rafael Lopez Torres <rltorres@dap.es>
2019-12-02 13:49:33 +0100

    Cambio estilo del label
    - Mas grande y claro
    - En los puntos se pone por encima. No tapa el punto.
    - Se escala en función del zoom

--- src/ogv/layer/ThematicLayer.js
+++ src/ogv/layer/ThematicLayer.js
@@ -3,118 +3,116 @@ import LayerTypeName from './LayerTypeName';
 import { LayerStyle } from '../style/LayerStyle';
 import { ThematicLayerLegend } from '../legend/ThematicLayerLegend';
 
-const FONT = 'bold 12px "Open Sans", "Arial Unicode MS", "sans-serif"';
 export class ThematicLayer extends BaseLayer {
-  constructor () {
-    super([], LayerTypeName.THEMATIC_LAYER);
-    this._style = new LayerStyle();
-  }
-
-  setSetting (layerSetting) {
-    this._map = layerSetting.map;
-    this._attribute = layerSetting[LayerTypeName.THEMATIC_LAYER].attribute;
-    this._label = layerSetting[LayerTypeName.THEMATIC_LAYER].label;
-    this._rate = layerSetting[LayerTypeName.THEMATIC_LAYER].rate;
-  }
-
-  setFeatures (features) {
-    super.setFeatures(features);
-    this.createCategories();
-    this.setLayerLegend(new ThematicLayerLegend({
-      categories: this._rule,
-      parent: this._map.getOverlayContainerStopEvent()
-    }));
-  }
-
-  createCategories () {
-    this._rule = this.createThematicRule();
-    const keys = Object.keys(this._rule);
-    this._style.setTotalColorPalette(keys.length);
-    const numberKeys = this.convertToNumber(keys);
-    this._rule = this.getOrderlyRule(numberKeys);
-  }
-
-  getLayer () {
-    super.getLayer();
-    this._layer.setStyle((feature) => {
-      const category = this._rule[feature.get(this._attribute)];
-
-      if (category === undefined) {
-        return null;
-      }
-
-      const style = category.style;
-      if (style !== undefined) {
-        if (this._label !== undefined) {
-          style.getText().setFont(FONT);
-          style.getText().setText('' + feature.get(this._label));
-        }
-
-        if (this._rate !== undefined && this._rate !== '') { style.getImage().setRadius(7 + parseInt(feature.get(this._attribute)) / this._rate); }
-      }
-
-      return style;
-    });
-
-    return this._layer;
-  }
-
-  createThematicRule () {
-    const rule = {};
-
-    this._features.forEach((feature) => {
-      const value = feature.get(this._attribute);
-
-      if (value !== null && !(value in rule)) {
-        rule[value] = {
-          count: 1
-        };
-      } else if (value !== null && (value in rule)) {
-        let count = parseInt(rule[value].count);
-        count = count + 1;
-        rule[value].count = count;
-      }
-    });
-
-    return rule;
-  }
-
-  convertToNumber (keys) {
-    var isNumeric = keys.every(function (e) {
-      return !isNaN(e);
-    });
-
-    if (isNumeric) {
-      return keys.map(Number);
-    } else {
-      return keys;
+    constructor() {
+        super([], LayerTypeName.THEMATIC_LAYER);
+        this._style = new LayerStyle();
     }
-  }
 
-  getOrderlyRule (keys) {
-    const orderlyRule = {};
-    const orderedKeys = keys.sort(function (a, b) {
-      if (a > b) {
-        return 1;
-      }
-      if (a < b) {
-        return -1;
-      }
+    setSetting(layerSetting) {
+        this._map = layerSetting.map;
+        this._attribute = layerSetting[LayerTypeName.THEMATIC_LAYER].attribute;
+        this._label = layerSetting[LayerTypeName.THEMATIC_LAYER].label;
+        this._rate = layerSetting[LayerTypeName.THEMATIC_LAYER].rate;
+    }
 
-      return 0;
-    });
+    setFeatures(features) {
+        super.setFeatures(features);
+        this.createCategories();
+        this.setLayerLegend(new ThematicLayerLegend({
+            categories: this._rule,
+            parent: this._map.getOverlayContainerStopEvent()
+        }));
+    }
 
-    orderedKeys.forEach((key) => {
-      const ruleValue = this._rule[key];
-      if (ruleValue !== undefined) {
-        orderlyRule['' + key] = {
-          count: ruleValue.count,
-          style: this._style.getNextStyle()
-        };
-      }
-    });
+    createCategories() {
+        this._rule = this.createThematicRule();
+        const keys = Object.keys(this._rule);
+        this._style.setTotalColorPalette(keys.length);
+        const numberKeys = this.convertToNumber(keys);
+        this._rule = this.getOrderlyRule(numberKeys);
+    }
 
-    orderlyRule.orderedKeys = orderedKeys;
-    return orderlyRule;
-  }
+    getLayer() {
+        super.getLayer();
+        this._layer.setStyle((feature) => {
+            const category = this._rule[feature.get(this._attribute)];
+
+            if (category === undefined) {
+                return null;
+            }
+
+            const style = category.style;
+            if (style !== undefined) {
+                if (this._label !== undefined) {
+                    style.getText().setText(this.stringDivider('' + feature.get(this._label), 16, '\n'));
+                }
+
+                if (this._rate !== undefined && this._rate !== '') { style.getImage().setRadius(7 + parseInt(feature.get(this._attribute)) / this._rate); }
+            }
+
+            return style;
+        });
+
+        return this._layer;
+    }
+
+    createThematicRule() {
+        const rule = {};
+
+        this._features.forEach((feature) => {
+            const value = feature.get(this._attribute);
+
+            if (value !== null && !(value in rule)) {
+                rule[value] = {
+                    count: 1
+                };
+            } else if (value !== null && (value in rule)) {
+                let count = parseInt(rule[value].count);
+                count = count + 1;
+                rule[value].count = count;
+            }
+        });
+
+        return rule;
+    }
+
+    convertToNumber(keys) {
+        var isNumeric = keys.every(function(e) {
+            return !isNaN(e);
+        });
+
+        if (isNumeric) {
+            return keys.map(Number);
+        } else {
+            return keys;
+        }
+    }
+
+    getOrderlyRule(keys) {
+        const orderlyRule = {};
+        const orderedKeys = keys.sort(function(a, b) {
+            if (a > b) {
+                return 1;
+            }
+            if (a < b) {
+                return -1;
+            }
+
+            return 0;
+        });
+
+        orderedKeys.forEach((key) => {
+            const ruleValue = this._rule[key];
+            if (ruleValue !== undefined) {
+                orderlyRule['' + key] = {
+                    count: ruleValue.count,
+                    style: this._style.getNextStyle()
+                };
+            }
+        });
+
+        orderlyRule.orderedKeys = orderedKeys;
+        return orderlyRule;
+    }
 }
--- src/ogv/style/LayerStyle.js
+++ src/ogv/style/LayerStyle.js
@@ -9,66 +9,68 @@ import colorPaletteGenerator from './ColorPaletteGenerator';
 const DEFAULT_RADIUS = 7;
 
 export class LayerStyle {
-  getNextStyle () {
-    const color = colorPaletteGenerator.getNextColor();
-    return this.getStyle(color, color, color);
-  }
+    getNextStyle() {
+        const color = colorPaletteGenerator.getNextColor();
+        return this.getStyle(color, color, color);
+    }
 
-  getStyle (fillColor, strokeColor, pointFillColor) {
-    const style = new Style({
-      fill: new Fill({
-        color: fillColor
-      }),
-      stroke: new Stroke({
-        color: strokeColor,
-        width: 3
-      }),
-      text: new Text({
-        // font: font,
-        fill: new Fill({
-          color: '#000'
-        }),
-        stroke: new Stroke({
-          color: '#fff',
-          width: 3
-        })
-      }),
-      image: new Circle({
-        radius: (this._radius === undefined || this._radius === '') ? DEFAULT_RADIUS : this._radius,
-        fill: new Fill({
-          color: pointFillColor
-        }),
-        stroke: new Stroke({
-          color: strokeColor,
-          width: 3
-        })
-      })
-    });
-    return style;
-  }
+    getStyle(fillColor, strokeColor, pointFillColor) {
+        const style = new Style({
+            fill: new Fill({
+                color: fillColor
+            }),
+            stroke: new Stroke({
+                color: strokeColor,
+                width: 3
+            }),
+            text: this.createTextStyle(),
+            image: new Circle({
+                radius: (this._radius === undefined || this._radius === '') ? DEFAULT_RADIUS : this._radius,
+                fill: new Fill({
+                    color: pointFillColor
+                }),
+                stroke: new Stroke({
+                    color: strokeColor,
+                    width: 3
+                })
+            })
+        });
+        return style;
+    }
 
-  getHexNextColor () {
-    var rgb = colorPaletteGenerator.getNextColor();
-    rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
-    return (rgb && rgb.length === 4) ? '#' +
+    getHexNextColor() {
+        var rgb = colorPaletteGenerator.getNextColor();
+        rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+        return (rgb && rgb.length === 4) ? '#' +
             ('0' + parseInt(rgb[1], 10).toString(16)).slice(-2) +
             ('0' + parseInt(rgb[2], 10).toString(16)).slice(-2) +
             ('0' + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
-  }
+    }
 
-  getNextColor () {
-    return colorPaletteGenerator.getNextColor();
-  }
+    getNextColor() {
+        return colorPaletteGenerator.getNextColor();
+    }
 
-  setColorPalette (colors) {
-    colorPaletteGenerator.setColorPalette(colors);
-  }
+    setColorPalette(colors) {
+        colorPaletteGenerator.setColorPalette(colors);
+    }
 
-  setTotalColorPalette (total) {
-    colorPaletteGenerator.setColors(undefined, undefined, total);
-  }
+    setTotalColorPalette(total) {
+        colorPaletteGenerator.setColors(undefined, undefined, total);
+    }
 
-  setRadius (radius) {
-    this._radius = radius;
-  }
+    setRadius(radius) {
+        this._radius = radius;
+    }
+
+    createTextStyle() {
+        return new Text({
+            overflow: true,
+            textBaseline: 'bottom',
+            font: 'Arial',
+            scale: 1.5,
+            fill: new Fill({ color: '#000' }),
+            stroke: new Stroke({ color: '#fff', width: 3 })
+        });
+    }
 }
