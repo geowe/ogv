5b5f7ad3ff472018296e2b931c905dfcd5e8ca52
José María Martínez Luna <jmmluna@gmail.com>
2019-12-17 16:02:20 +0100

    Versión 2.14.3. Mejora de leyendas por SLD. Parámetro attribute-no-null

--- src/ogv/layer/BaseLayer.js
+++ src/ogv/layer/BaseLayer.js
@@ -2,7 +2,6 @@ import VectorSource from 'ol/source/Vector';
 import Feature from 'ol/Feature';
 import LayerTypeName from './LayerTypeName';
 import VectorImageLayer from 'ol/layer/VectorImage';
-import View from 'ol/View';
 
 /**
  * @classdesc
@@ -10,172 +9,161 @@ import View from 'ol/View';
  * @abstract
  */
 export class BaseLayer {
-    /**
+  /**
      * @param {Array<Feature>} features
      */
-    constructor(features, layerTypeName) {
-        this._layerTypeName = layerTypeName;
-        /**
+  constructor (features, layerTypeName) {
+    this._layerTypeName = layerTypeName;
+    /**
          * @type {Array<Feature>}
          */
 
-        this._features = features;
+    this._features = features;
 
-        /**
+    /**
          * @type {import("ol/layer/Layer").default}
          */
-        this._layer = undefined;
-    }
+    this._layer = undefined;
+  }
 
-    getLayerTypeName() {
-        return this._layerTypeName;
-    }
+  getLayerTypeName () {
+    return this._layerTypeName;
+  }
 
-    /**
+  /**
      * @returns {Array<Feature>}
      */
-    getFeatures() {
-        return this._features;
-    }
+  getFeatures () {
+    return this._features;
+  }
 
-    /**
+  /**
      * @param {Array<Feature>} features
      */
-    setFeatures(features) {
-        this._features = features;
-    }
+  setFeatures (features) {
+    this._features = features;
+  }
 
-    /**
+  /**
      * @returns {VectorSource}
      */
-    getVectorSource(features) {
-        return new VectorSource({
-            features: features
-        });
-    }
+  getVectorSource (features) {
+    return new VectorSource({
+      features: features
+    });
+  }
 
-    /**
+  /**
      * @returns {void}
      */
-    convertToSimpleFeatures() {
-        this._features = this.toSimpleFeatures(this.getFeatures(), undefined);
-    }
+  convertToSimpleFeatures () {
+    this._features = this.toSimpleFeatures(this.getFeatures(), undefined);
+  }
 
-    /**
+  /**
      * @param {Feature} featureTemplate
      * @param {Array<Feature>|undefined} features
      * @returns {Array<Feature>}
      */
 
-    toSimpleFeatures(features, featureTemplate) {
-        var simpleFeatures = [];
+  toSimpleFeatures (features, featureTemplate) {
+    var simpleFeatures = [];
 
-        features.forEach((feature) => {
-            var geom;
-            if (!(feature instanceof Feature)) {
-                geom = feature;
-                feature = featureTemplate.clone();
-                feature.setGeometry(geom);
-            }
+    features.forEach((feature) => {
+      var geom;
+      if (!(feature instanceof Feature)) {
+        geom = feature;
+        feature = featureTemplate.clone();
+        feature.setGeometry(geom);
+      }
 
-            geom = feature.getGeometry();
+      geom = feature.getGeometry();
 
-            if (geom.getType() === 'GeometryCollection') { simpleFeatures = simpleFeatures.concat(this.toSimpleFeatures(geom.getGeometries(), feature)); } else if (geom.getType() === 'MultiPolygon') { simpleFeatures = simpleFeatures.concat(this.toSimpleFeatures(geom.getPolygons(), feature)); } else if (geom.getType() === 'MultiPoint') { simpleFeatures = simpleFeatures.concat(this.toSimpleFeatures(geom.getPoints(), feature)); } else { simpleFeatures.push(feature); }
-        });
+      if (geom.getType() === 'GeometryCollection') { simpleFeatures = simpleFeatures.concat(this.toSimpleFeatures(geom.getGeometries(), feature)); } else if (geom.getType() === 'MultiPolygon') { simpleFeatures = simpleFeatures.concat(this.toSimpleFeatures(geom.getPolygons(), feature)); } else if (geom.getType() === 'MultiPoint') { simpleFeatures = simpleFeatures.concat(this.toSimpleFeatures(geom.getPoints(), feature)); } else { simpleFeatures.push(feature); }
+    });
 
-        return simpleFeatures;
+    return simpleFeatures;
+  }
+
+  getEmptyLayer (layerName) {
+    this.prepareLayer(layerName);
+    return this.getLayer();
+  }
+
+  getLayerFromFeatures (layerName) {
+    this.prepareLayer(layerName);
+    this._layer.getSource().addFeatures(this.getFeatures());
+    return this.getLayer();
+  }
+
+  prepareLayer (layerName) {
+    /* this._layer = new Vector({
+                        source: this.getVectorSource([])
+                    }); */
+
+    this._layer = new VectorImageLayer({
+      imageRatio: 2,
+      source: new VectorSource({})
+    });
+
+    this._layer.set('name', layerName);
+  }
+
+  setLayerLegend (layerLegend) {
+    this._layerLegend = layerLegend;
+  }
+
+  getLayerLegend () {
+    return this._layerLegend;
+  }
+
+  prepareLegend (mapSetting) {
+    if (mapSetting.legend === undefined) { return; }
+
+    this._mapSetting = mapSetting;
+    this._layerName = this._layer.get('name').split('.')[0];
+
+    if (this.getLayerTypeName() !== LayerTypeName.SLD_LAYER) {
+      this._layerLegend.prepareColor();
+      this.onLayerLegend();
     }
+  }
 
-    getEmptyLayer(layerName) {
-        this.prepareLayer(layerName);
-        return this.getLayer();
-    }
+  onLayerLegend () {
+    this._layerLegend.prepareLegendInfo(this._mapSetting);
+    this._mapSetting.legend.layerTotalFeatures = this.getFeatures().length;
+    this._mapSetting.legend.layers[this._layerName] = this._layer;
+    this._layerLegend.show(this._mapSetting, this._layerName);
+  }
 
-    getLayerFromFeatures(layerName) {
-        this.prepareLayer(layerName);
-        this._layer.getSource().addFeatures(this.getFeatures());
-        return this.getLayer();
-    }
+  getLayer () {
+    return this._layer;
+  }
 
-    prepareLayer(layerName) {
-        /* this._layer = new Vector({
-                    source: this.getVectorSource([])
-                }); */
-
-        this._layer = new VectorImageLayer({
-            imageRatio: 2,
-            source: new VectorSource({})
-        });
-
-        this._layer.set('name', layerName);
-    }
-
-    setLayerLegend(layerLegend) {
-        this._layerLegend = layerLegend;
-    }
-
-    getLayerLegend() {
-        return this._layerLegend;
-    }
-
-    prepareLegend(mapSetting) {
-        if (mapSetting.legend === undefined) { return; }
-
-        const layerName = this._layer.get('name').split('.')[0];
-
-        if (this.getLayerTypeName() === LayerTypeName.SLD_LAYER) {
-            this.prepareSLDLegend(mapSetting, layerName);
-        } else {
-            this._layerLegend.prepareColor();
-            this.onLayerLegend(mapSetting, layerName);
-        }
-    }
-
-    prepareSLDLegend(mapSetting, layerName) {
-        const onLayerLegend = setTimeout(() => {
-            if (this._layerLegend !== undefined) {
-                clearTimeout(onLayerLegend);
-                this.onLayerLegend(mapSetting, layerName);
-            }
-        }, 500);
-    }
-
-    onLayerLegend(mapSetting, layerName) {
-        this._layerLegend.prepareLegendInfo(mapSetting);
-        mapSetting.legend.layerTotalFeatures = this.getFeatures().length;
-        mapSetting.legend.layers[layerName] = this._layer;
-        this._layerLegend.show(mapSetting, layerName);
-    }
-
-    getLayer() {
-        return this._layer;
-    }
-
-    /**
+  /**
      * Divide una cadena en varias líneas
      * http://stackoverflow.com/questions/14484787/wrap-text-in-javascript
      * @param {*} str cadena a dividir
      * @param {*} width ancho de carateres
      * @param {*} spaceReplacer cadena que reemplaza
      */
-    wrapText(str, width, spaceReplacer) {
-        if (str.length > width) {
-            var p = width;
-            while (p > 0 && (str[p] != ' ' && str[p] != '-')) {
-                p--;
-            }
-            if (p > 0) {
-                var left;
-                if (str.substring(p, p + 1) == '-') {
-                    left = str.substring(0, p + 1);
-                } else {
-                    left = str.substring(0, p);
-                }
-                var right = str.substring(p + 1);
-                return left + spaceReplacer + this.wrapText(right, width, spaceReplacer);
-            }
+  wrapText (str, width, spaceReplacer) {
+    if (str.length > width) {
+      var p = width;
+      while (p > 0 && (str[p] != ' ' && str[p] != '-')) {
+        p--;
+      }
+      if (p > 0) {
+        var left;
+        if (str.substring(p, p + 1) == '-') {
+          left = str.substring(0, p + 1);
+        } else {
+          left = str.substring(0, p);
         }
-        return str;
+        var right = str.substring(p + 1);
+        return left + spaceReplacer + this.wrapText(right, width, spaceReplacer);
+      }
     }
-
+    return str;
+  }
 }
--- src/ogv/layer/ThematicLayer.js
+++ src/ogv/layer/ThematicLayer.js
@@ -4,115 +4,115 @@ import { LayerStyle } from '../style/LayerStyle';
 import { ThematicLayerLegend } from '../legend/ThematicLayerLegend';
 
 export class ThematicLayer extends BaseLayer {
-    constructor() {
-        super([], LayerTypeName.THEMATIC_LAYER);
-        this._style = new LayerStyle();
-    }
+  constructor () {
+    super([], LayerTypeName.THEMATIC_LAYER);
+    this._style = new LayerStyle();
+  }
 
-    setSetting(layerSetting) {
-        this._map = layerSetting.map;
-        this._attribute = layerSetting[LayerTypeName.THEMATIC_LAYER].attribute;
-        this._label = layerSetting[LayerTypeName.THEMATIC_LAYER].label;
-        this._rate = layerSetting[LayerTypeName.THEMATIC_LAYER].rate;
-    }
+  setSetting (layerSetting) {
+    this._map = layerSetting.map;
+    this._attribute = layerSetting[LayerTypeName.THEMATIC_LAYER].attribute;
+    this._label = layerSetting[LayerTypeName.THEMATIC_LAYER].label;
+    this._rate = layerSetting[LayerTypeName.THEMATIC_LAYER].rate;
+  }
 
-    setFeatures(features) {
-        super.setFeatures(features);
-        this.createCategories();
-        this.setLayerLegend(new ThematicLayerLegend({
-            categories: this._rule,
-            parent: this._map.getOverlayContainerStopEvent()
-        }));
-    }
+  setFeatures (features) {
+    super.setFeatures(features);
+    this.createCategories();
+    this.setLayerLegend(new ThematicLayerLegend({
+      categories: this._rule,
+      parent: this._map.getOverlayContainerStopEvent()
+    }));
+  }
 
-    createCategories() {
-        this._rule = this.createThematicRule();
-        const keys = Object.keys(this._rule);
-        this._style.setTotalColorPalette(keys.length);
-        const numberKeys = this.convertToNumber(keys);
-        this._rule = this.getOrderlyRule(numberKeys);
-    }
+  createCategories () {
+    this._rule = this.createThematicRule();
+    const keys = Object.keys(this._rule);
+    this._style.setTotalColorPalette(keys.length);
+    const numberKeys = this.convertToNumber(keys);
+    this._rule = this.getOrderlyRule(numberKeys);
+  }
 
-    getLayer() {
-        super.getLayer();
-        this._layer.setStyle((feature) => {
-            const category = this._rule[feature.get(this._attribute)];
+  getLayer () {
+    super.getLayer();
+    this._layer.setStyle((feature) => {
+      const category = this._rule[feature.get(this._attribute)];
 
-            if (category === undefined) {
-                return null;
-            }
+      if (category === undefined) {
+        return null;
+      }
 
-            const style = category.style;
-            if (style !== undefined) {
-                if (this._label !== undefined) {
-                    style.getText().setText(this.wrapText('' + feature.get(this._label), 16, '\n'));
-                }
-
-                if (this._rate !== undefined && this._rate !== '') { style.getImage().setRadius(7 + parseInt(feature.get(this._attribute)) / this._rate); }
-            }
-
-            return style;
-        });
-
-        return this._layer;
-    }
-
-    createThematicRule() {
-        const rule = {};
-
-        this._features.forEach((feature) => {
-            const value = feature.get(this._attribute);
-
-            if (value !== null && !(value in rule)) {
-                rule[value] = {
-                    count: 1
-                };
-            } else if (value !== null && (value in rule)) {
-                let count = parseInt(rule[value].count);
-                count = count + 1;
-                rule[value].count = count;
-            }
-        });
-
-        return rule;
-    }
-
-    convertToNumber(keys) {
-        var isNumeric = keys.every(function(e) {
-            return !isNaN(e);
-        });
-
-        if (isNumeric) {
-            return keys.map(Number);
-        } else {
-            return keys;
+      const style = category.style;
+      if (style !== undefined) {
+        if (this._label !== undefined) {
+          style.getText().setText(this.wrapText('' + feature.get(this._label), 16, '\n'));
         }
+
+        if (this._rate !== undefined && this._rate !== '') { style.getImage().setRadius(7 + parseInt(feature.get(this._attribute)) / this._rate); }
+      }
+
+      return style;
+    });
+
+    return this._layer;
+  }
+
+  createThematicRule () {
+    const rule = {};
+
+    this._features.forEach((feature) => {
+      const value = feature.get(this._attribute);
+
+      if (value !== null && !(value in rule)) {
+        rule[value] = {
+          count: 1
+        };
+      } else if (value !== null && (value in rule)) {
+        let count = parseInt(rule[value].count);
+        count = count + 1;
+        rule[value].count = count;
+      }
+    });
+
+    return rule;
+  }
+
+  convertToNumber (keys) {
+    var isNumeric = keys.every(function (e) {
+      return !isNaN(e);
+    });
+
+    if (isNumeric) {
+      return keys.map(Number);
+    } else {
+      return keys;
     }
+  }
 
-    getOrderlyRule(keys) {
-        const orderlyRule = {};
-        const orderedKeys = keys.sort(function(a, b) {
-            if (a > b) {
-                return 1;
-            }
-            if (a < b) {
-                return -1;
-            }
+  getOrderlyRule (keys) {
+    const orderlyRule = {};
+    const orderedKeys = keys.sort(function (a, b) {
+      if (a > b) {
+        return 1;
+      }
+      if (a < b) {
+        return -1;
+      }
 
-            return 0;
-        });
+      return 0;
+    });
 
-        orderedKeys.forEach((key) => {
-            const ruleValue = this._rule[key];
-            if (ruleValue !== undefined) {
-                orderlyRule['' + key] = {
-                    count: ruleValue.count,
-                    style: this._style.getNextStyle()
-                };
-            }
-        });
+    orderedKeys.forEach((key) => {
+      const ruleValue = this._rule[key];
+      if (ruleValue !== undefined) {
+        orderlyRule['' + key] = {
+          count: ruleValue.count,
+          style: this._style.getNextStyle()
+        };
+      }
+    });
 
-        orderlyRule.orderedKeys = orderedKeys;
-        return orderlyRule;
-    }
+    orderlyRule.orderedKeys = orderedKeys;
+    return orderlyRule;
+  }
 }
